32cdecbb84dd08378484025c97cb9067302fc294
1Muel <samuelcaliel69@gmail.com>
2023-09-05 10:27:51 -0300

    rbggbrtbgrtbivervfg

--- script.js
+++
@@ -1,7 +0,0 @@
-function btn_onclick_entrar(){
-    let email = document.getElementById("email").value;
-    let senha = document.getElementById("senha").value;
-
-    localStorage.setItem("Email",email);
-    localStorage.setItem("Senha",senha);
-}
